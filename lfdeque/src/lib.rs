92c4cb2a94d559434700b7ce6ab34eb4de3deaf5
rmn7 <armanit.garg@imply.io>
2022-01-03 01:27:40 +0530

    exapandable lfdq

--- lfdeque/src/lib.rs
+++ lfdeque/src/lib.rs
@@ -1,56 +1,65 @@
-use std::sync::Arc;
+use std::mem;
+use std::sync::{Arc, Weak};
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::cell::UnsafeCell;
-mod seat;
-use seat::Seat;
 
 #[derive(Debug, Clone)]
 pub struct LFQueue<T> {
     top: Arc<AtomicUsize>,
     bottom: Arc<AtomicUsize>,
-    q: Arc<UnsafeCell<Vec<Option<Seat<T>>>>>,
+    q: Arc<UnsafeCell<Vec<Option<Arc<T>>>>>,
 }
 
 impl<T: std::fmt::Debug> LFQueue<T> {
     pub fn new(capacity: usize) -> Self {
+        Self {
+            top: Arc::new(AtomicUsize::new(0)),
+            bottom: Arc::new(AtomicUsize::new(0)),
+            q: LFQueue::new_q(capacity)
+        }
+    }
+
+    fn new_q(capacity: usize) -> Arc<UnsafeCell<Vec<Option<Arc<T>>>>> {
         let q = Arc::new(UnsafeCell::new(Vec::new()));
         let temp = q.get();
         unsafe {
             (*temp).resize_with(capacity, || None);
         }
-
-        Self {
-            top: Arc::new(AtomicUsize::new(0)),
-            bottom: Arc::new(AtomicUsize::new(0)),
-            q
-        }
+        q
     }
 
-    fn get_elem(&mut self, idx: usize) -> Option<Seat<T>> {
+    fn get_elem(&mut self, idx: usize) -> Weak<T> {
         let q = self.q.get();
         unsafe {
             let len = (*q).len();
             let elem = &(*q)[idx % len];
-            elem.as_ref().cloned()
+            Arc::downgrade(&elem.clone().unwrap())
         }
     }
 
-    pub fn push(&mut self, elem: T) -> bool {
+    pub fn push(&mut self, elem: T) {
         let b = self.bottom.load(Ordering::SeqCst);
         let t = self.top.load(Ordering::SeqCst);
 
-        let q = self.q.get();
+        let mut q = self.q.get();
         unsafe {
-            let len = (*q).len();
+            let mut len = (*q).len();
             if b - t >= len - 1 {
-                return false
+                let mut new_q = LFQueue::new_q(len * 2);
+                let copy = new_q.get();
+                for i in t..b {
+                    (*copy)[i] = (&(*q)[i % len]).clone();
+                }
+
+                mem::swap(&mut self.q, &mut new_q);
+                q = self.q.get();
+                len *= 2;
             }
 
-            (*q)[b % len] = Some(Seat::new(elem));
+            (*q)[b % len] = Some(Arc::new(elem));
         }
 
         self.bottom.store(b + 1, Ordering::SeqCst);
-        true
     }
 
     pub fn pop(&mut self) -> Option<T> {
@@ -64,12 +73,14 @@ impl<T: std::fmt::Debug> LFQueue<T> {
         
         let elem = self.get_elem(b);
         if b > t {
-            return elem.map(Seat::take)
+            let arc = Weak::upgrade(&elem);
+            return Some(Arc::try_unwrap(arc.unwrap()).unwrap());
         }
 
         self.bottom.store(t + 1, Ordering::SeqCst);
         if self.top.compare_exchange(t, t+1, Ordering::SeqCst, Ordering::SeqCst).is_ok() {
-            return elem.map(Seat::take);
+            let arc = Weak::upgrade(&elem);
+            return Some(Arc::try_unwrap(arc.unwrap()).unwrap());
         }
 
         None
@@ -82,7 +93,8 @@ impl<T: std::fmt::Debug> LFQueue<T> {
         if b <= t { return None; }
         let elem = self.get_elem(t);
         if self.top.compare_exchange(t, t+1, Ordering::SeqCst, Ordering::SeqCst).is_ok() {
-            return elem.map(Seat::take)
+            let arc = Weak::upgrade(&elem).unwrap();
+            return Some(Arc::try_unwrap(arc).unwrap());
         }
 
         None
@@ -93,84 +105,200 @@ unsafe impl<T> Send for LFQueue<T> where T: Send {}
 unsafe impl<T> Sync for LFQueue<T> where T: Send {}
 
 #[test]
-fn test_init() {
-    use std::thread;
+fn test() {
+    let mut q = LFQueue::new(3);
 
-    let push_elemts = [1, 2, 93, 104, 2044];
-    let count =  100000;
-    let thread_count = 7;
+    q.push(1);
+    q.push(2);
 
-    let s: usize = push_elemts.iter().sum();
-    let expected_sum = count * s;
-    let expected_elems = count * push_elemts.len();
-    let mut threads = Vec::new();
-
-    let mut q = LFQueue::new(5);
-    let cur = Arc::new(AtomicUsize::new(0));
-
-    for i in 0..thread_count {
-        let mut q_clone = q.clone();
-        let cur_clone = cur.clone();
-    
-        let t = thread::spawn(move || {
-            let mut here = 0;
-            loop {
-                let e = q_clone.steal();
-                match e {
-                    None => {},
-                    Some(e) => { 
-                        let _ = cur_clone.fetch_add(e, Ordering::SeqCst);
-                        here += 1;
-                    }
-                }
-        
-                let c = cur_clone.load(Ordering::SeqCst);
-                if c == expected_sum { break; }
-            }
-
-            (i, here)
-        });
-
-        threads.push(t);
+    let a = q.q.get();
+    unsafe {
+        println!("{:?}", *a);
     }
 
-    for _ in 0..count {
-        for e in push_elemts {
-            let mut r = q.push(e);
-            while !r {
-                r = q.push(e);
-            }
-        }
-    }
+    println!("{:?}", q.steal());
+    println!("{:?}", q.steal());
 
-    let mut here = 0;
-    loop {
-        let e = q.pop();
-        match e {
-            None => {},
-            Some(e) => {
-                let _ = cur.fetch_add(e, Ordering::SeqCst); 
-                here += 1;
-            }
-        }
-        println!("main {:?}", e);
-        let c = cur.load(Ordering::SeqCst);
-        if c == expected_sum { break; }
-    }
+    // q.push(4);
 
-    let mut exec_count = 0;
-    println!("thrd\texecs");
-    for t in threads {
-        let (t_id, count) = t.join().unwrap();
-        println!("{}\t{}", t_id, count);
-        exec_count += count;
-    }
-    println!("main\t{}", here);
-    exec_count += here;
-    println!("total\t{}", exec_count);
+    // unsafe {
+    //     println!("{:?}", *a);
+    // }
 
-    assert_eq!(cur.load(Ordering::SeqCst), expected_sum);
-    assert_eq!(exec_count, expected_elems);
-    assert_eq!(q.pop(), None);
-    assert_eq!(q.steal(), None);
+    // q.push(5);
+
+    // unsafe {
+    //     println!("{:?}", *a);
+    // }
+
+    // println!("{:?}", q.steal());
+    // println!("{:?}", q.steal());
+    // println!("{:?}", q.steal());
+
+    // unsafe {
+    //     println!("{:?}", *a);
+    // }
+
+    q.pop();
 }
+
+
+// #[test]
+// fn test_init() {
+//     use std::thread;
+
+//     let push_elemts = [1, 2, 93, 104, 2044];
+//     let count =  100000;
+//     let thread_count = 3;
+
+//     let s: usize = push_elemts.iter().sum();
+//     let expected_sum = count * s;
+//     let expected_elems = count * push_elemts.len();
+//     let mut threads = Vec::new();
+
+//     let mut q = LFQueue::new(100);
+//     let cur = Arc::new(AtomicUsize::new(0));
+
+//     for i in 0..thread_count {
+//         let mut q_clone = q.clone();
+//         let cur_clone = cur.clone();
+    
+//         let t = thread::spawn(move || {
+//             let mut here = 0;
+//             loop {
+//                 let e = q_clone.steal();
+//                 match e {
+//                     None => {},
+//                     Some(e) => { 
+//                         let _ = cur_clone.fetch_add(e, Ordering::SeqCst);
+//                         here += 1;
+//                     }
+//                 }
+        
+//                 let c = cur_clone.load(Ordering::SeqCst);
+//                 if c == expected_sum { break; }
+//             }
+
+//             (i, here)
+//         });
+
+//         threads.push(t);
+//     }
+
+//     for _ in 0..count {
+//         for e in push_elemts {
+//             q.push(e);
+//         }
+//     }
+
+//     let mut here = 0;
+//     loop {
+//         let e = q.pop();
+//         match e {
+//             None => {},
+//             Some(e) => {
+//                 let _ = cur.fetch_add(e, Ordering::SeqCst); 
+//                 here += 1;
+//             }
+//         }
+//         let c = cur.load(Ordering::SeqCst);
+//         if c == expected_sum { break; }
+//     }
+
+//     let mut exec_count = 0;
+//     println!("thrd\texecs");
+//     for t in threads {
+//         let (t_id, count) = t.join().unwrap();
+//         println!("{}\t{}", t_id, count);
+//         exec_count += count;
+//     }
+//     println!("main\t{}", here);
+//     exec_count += here;
+//     println!("total\t{}", exec_count);
+
+//     assert_eq!(cur.load(Ordering::SeqCst), expected_sum);
+//     assert_eq!(exec_count, expected_elems);
+//     assert_eq!(q.pop(), None);
+//     assert_eq!(q.steal(), None);
+// }
+
+// #[test]
+// fn test_init() {
+//     use std::thread;
+
+//     let push_elemts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+//     let count =  100000;
+//     let thread_count = 10;
+// //     let count =  1;
+// //     let thread_count = 5;
+
+//     let s: usize = push_elemts.iter().sum();
+//     let expected_sum = count * s;
+//     let expected_elems = count * push_elemts.len();
+//     let mut threads = Vec::new();
+
+//     let mut q = LFQueue::new(1);
+//     let cur = Arc::new(AtomicUsize::new(0));
+
+//     for i in 0..thread_count {
+//         let mut q_clone = q.clone();
+//         let cur_clone = cur.clone();
+    
+//         let t = thread::spawn(move || {
+//             let mut here = 0;
+//             loop {
+//                 let e = q_clone.steal();
+//                 match e {
+//                     None => {},
+//                     Some(e) => { 
+//                         let _ = cur_clone.fetch_add(e, Ordering::SeqCst);
+//                         here += 1;
+//                     }
+//                 }
+        
+//                 let c = cur_clone.load(Ordering::SeqCst);
+//                 if c == expected_sum { break; }
+//             }
+
+//             (i, here)
+//         });
+
+//         threads.push(t);
+//     }
+
+//     for _ in 0..count {
+//         for e in push_elemts {
+//             q.push(e);
+//         }
+//     }
+
+//     let mut here = 0;
+//     // loop {
+//     //     let e = q.pop();
+//     //     match e {
+//     //         None => {},
+//     //         Some(e) => {
+//     //             let _ = cur.fetch_add(e, Ordering::SeqCst); 
+//     //             here += 1;
+//     //         }
+//     //     }
+//     //     let c = cur.load(Ordering::SeqCst);
+//     //     if c == expected_sum { break; }
+//     // }
+
+//     let mut exec_count = 0;
+//     println!("thrd\texecs");
+//     for t in threads {
+//         let (t_id, count) = t.join().unwrap();
+//         println!("{}\t{}", t_id, count);
+//         exec_count += count;
+//     }
+//     println!("main\t{}", here);
+//     exec_count += here;
+//     println!("total\t{}", exec_count);
+
+//     assert_eq!(cur.load(Ordering::SeqCst), expected_sum);
+//     assert_eq!(exec_count, expected_elems);
+//     assert_eq!(q.pop(), None);
+//     assert_eq!(q.steal(), None);
+// }
--- lfdeque/src/seat.rs
+++ lfdeque/src/seat.rs
@@ -1,41 +1,75 @@
-use std::{mem::{self}, ptr::NonNull, ptr};
+// use std::{mem::{self}, ptr::NonNull, ptr, sync::atomic::{AtomicUsize, Ordering}};
 
-#[derive(Debug)]
-struct SeatInner<T> {
-    data: T
-}
+// #[derive(Debug)]
+// struct SeatInner<T> {
+//     data: Option<T>
+// }
 
-#[derive(Debug)]
-pub struct Seat<T> {
-    ptr: NonNull<SeatInner<T>>,
-}
+// impl<T> SeatInner<T> {
+//     fn new(data: T) -> Self {
+//         Self { data: Some(data) }
+//     }
+// }
 
-impl<T> Seat<T> {
-    pub fn new(data: T) -> Self {
-        let ptr = Box::new(SeatInner {data});
-        Self::from_inner(Box::leak(ptr).into())
-    }
+// impl<T> Default for SeatInner<T> {
+//     fn default() -> Self {
+//         Self { data: None }
+//     }
+// }
 
-    fn from_inner(ptr: NonNull<SeatInner<T>>) -> Self {
-        Self { ptr }
-    }
+// #[derive(Debug)]
+// pub struct Seat<T> {
+//     ptr: NonNull<SeatInner<T>>,
+//     rc: Arc<AtomicUsize>,
+// }
 
-    pub fn take(this: Self) -> T {
-        unsafe {
-            let elem = ptr::read(&this.ptr.as_ref().data);
+// impl<T> Seat<T> {
+//     pub fn sit(&mut self, data: T) {
+//         self.rc.fetch_add(1, Ordering::SeqCst);
+//         let ptr = Box::new(SeatInner::new(data));
+//         self.ptr = Box::leak(ptr).into();
+//     }
 
-            mem::forget(this);
+//     fn from_inner(ptr: NonNull<SeatInner<T>>) -> Self {
+//         Self { ptr, rc: Arc::new(AtomicUsize::new(0)) }
+//     }
 
-            elem
-        }        
-    }
-}
+//     pub fn take(&mut self, j: usize) -> T {
+//         self.rc.fetch_sub(1, Ordering::SeqCst);
+//         unsafe {
+//             let e = mem::replace(&mut self.ptr.as_mut().data, None);
+//             match e {
+//                 None => panic!("This should have been unreachable {}", j),
+//                 Some(e) => e
+//             }
+//         }
+//     }
+// }
 
-impl<T> Clone for Seat<T> {
-    fn clone(&self) -> Self {
-        Self::from_inner(self.ptr)
-    }
-}
+// impl<T> Drop for Seat<T> {
+//     fn drop(&mut self) {
+//         unsafe {
+//             let cou = self.rc.fetch_sub(1, Ordering::SeqCst);
+//             if cou == 0 {
+//                 ptr::drop_in_place(self.ptr.as_ptr());
+//             }
+//         }
+//     }
+// }
 
-unsafe impl<T> Send for Seat<T> where T: Send {}
-unsafe impl<T> Sync for Seat<T> where T: Send {}
+// impl<T> Clone for Seat<T> {
+//     fn clone(&self) -> Self {
+//         self.rc.fetch_add(1, Ordering::SeqCst);
+//         Self::from_inner(self.ptr)
+//     }
+// }
+
+// impl<T> Default for Seat<T> {
+//     fn default() -> Self {
+//         let ptr = Box::new(SeatInner::default());
+//         Self::from_inner(Box::leak(ptr).into())
+//     }
+// }
+
+// unsafe impl<T> Send for Seat<T> where T: Send {}
+// unsafe impl<T> Sync for Seat<T> where T: Send {}
